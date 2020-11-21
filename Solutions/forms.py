9ea553c53a24b9a76fdff2bee633fd00f38b8ec2
vaskovatk <vaskovatk@gmail.com>
2020-11-21 22:38:41 +0600

    change 21/11/20

--- Solutions/forms.py
+++ Solutions/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class SolForm (forms.Form):
-    solution = forms.CharField (label ="solution", widget=forms.Textarea)
+    solution = forms.CharField (label ="solution", widget=forms.Textarea(attrs={'class':'form_control'}))
--- Solutions/views.py
+++ Solutions/views.py
@@ -7,26 +7,29 @@ from django.shortcuts import HttpResponse
 def sol(request):
     return render(request, 'Solutions/sol.html')
 def solved(request):
-    if request.method =="POST":
+    if request.method =="GET":
+        form = forms.SolForm()
+        dict = { 'form': form
+        }
+        return render(request, 'checksol.html', context= dict)
+    elif request.method =="POST":
         form = forms.SolForm(request.POST)
         if form.is_valid():
-            solution  = request.POST['solution']
-            dict = {"form" : forms.SolForm}
+            # solution  = request.POST['solution']
+            dict = {"form" : form}
             dict['success'] = True
             dict['successing'] ='Form submitted'
             return render(request, "Solutions/checksoltrue.html", dict)
         else:
             dict ={}
-            dict['success'] = True
-            dict['successing'] = 'Form submitted'
             return render(request, "Solutions/checksolfalse.html", dict)
         # result = runpy._run_code(code=form, run_globals=None, mod_name=None,mod_spec=None, pkg_name=None, script_name=None)
         # if models.Task.realsolution == result:
         #     return render(request, 'Solutions/checksoltrue.html')
         # else:
         #     return render(request, 'Solutions/checksolfalse.html')
-    elif request.method == "GET":
-        return render(request, "Solutions/sol.html")
+    # elif request.method == "GET":
+    #     return render(request, "Solutions/sol.html")
 
 # Create your views here.
 # создать список задач на которые надо будет нажимать и вводить ответ, форма для решения не должна появлятся просто так а только после нажатия на вопрос после которого переадресация на форму
