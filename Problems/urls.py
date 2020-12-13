154c6a13a6c3b3e19a0c3340a9cec676bf39c1da
vaskovatk <vaskovatk@gmail.com>
2020-12-14 01:01:42 +0600

    change 13/12/20 works version add urls in problems add pk urls in problems add graphics in problems del buttons in header change task.html change tasks.html add new functions in Problems/views add first connect between Solutions and Problems

--- Problems/urls.py
+++ Problems/urls.py
@@ -2,9 +2,12 @@ from django.urls import path, include
 from . import views
 from django.views.generic import ListView, DetailView
 from .models import Task
+from Solutions import views as viewSol
 
 urlpatterns = [
     path('', views.allTasks, name='problems'),
-    # path('', ListView.as_view(queryset=Task.objects.all().order_by("-date")[:20], template_name="Problems/tasks.html")),
-    # path('<int:pk>/', DetailView.as_view(model=Task, template_name='Problems/task.html')),
+    # path('<int:pk>/',DetailView.as_view(model=Task, template_name='Problems/task.html')),
+    path('<int:pk>/', views.task, name ='task'),
+    path('solutions', viewSol.sol, name='solutions'),
+    path('solved', viewSol.solved, name='solved'),
 ]
--- Problems/views.py
+++ Problems/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Task
+from django.views.generic import ListView, DetailView
 
 def allTasks(request):
     tasks = Task.objects.order_by('date')[:50]
@@ -8,7 +9,13 @@ def allTasks(request):
     }
     return render(request, 'Problems/tasks.html', dict)
 
+def task (request, pk):
+    task = Task.objects.filter(id=pk)
+    dict = {'task': task}
+    return render(request, 'Problems/task.html', dict)
 
+# todo здесь запускать сравнение по персональному ключу в real solution с тем что лежит в отдельном файле user result
+# todo может быть здесь брать содержимое realsolution у pk и записывать его в файл а потом сравнивать с решением пользователя
 
 
 # # Create your views here.
