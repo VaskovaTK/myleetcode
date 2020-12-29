0aebd515177d2c8c22cdc9790074c897454b10d7
vaskovatk <vaskovatk@gmail.com>
2020-12-29 23:44:54 +0600

    change from 29/12/20 add tab function to form merge views delete clickhere create columns delete urls from Solutions

--- Solutions/urls.py
+++ Solutions/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.sol, name='solutions'),
-    path('solved', views.solved, name='solved'),
-
+    # path('', views.sol, name='solutions'),
+    # path('solved', views.sol, name='solved'),
+    # path('<int:pk>/', views.solved, name ='solvedpk'),
 ]
--- Solutions/views.py
+++ Solutions/views.py
@@ -1,26 +1,32 @@
 from django.shortcuts import render
 from . import forms
-from Problems import models
+from Problems import models as modelTask
 from django.shortcuts import HttpResponse
 import runProgram
 import saveToFile
 
-def sol(request):
-    return render(request, 'Solutions/sol.html')
-def solved(request):
-    if request.method =="GET":
-        form = forms.SolForm()
-        dict = { 'form': form
-        }
-        return render(request, 'checksol.html', context= dict)
-    elif request.method =="POST":
-        form = forms.SolForm(request.POST)
-        data = form.data['textarea']
-        save = saveToFile.Save()
-        filePath = save.saveToFile(data)
-        run = runProgram.RunProgram()
-        afterRun = run.runFile(filePath, 0)
-        dict = {"data" : data}
-        dict['afterRun'] = afterRun
-        # todo сравнивать значения с realsolution
-        return render(request, "Solutions/checksoltrue.html", dict)
+# def sol(request):
+#     return render(request, 'Solutions/sol.html')
+#
+# def solved(request, pk):
+    # if request.method =="GET":
+    #     form = forms.SolForm()
+    #     dict = { 'form': form,
+    #              'pk': str(pk)
+    #     }
+    #     return render(request, 'Solutions/solved.html', context= dict)
+    # elif request.method =="POST":
+    #     session = request.session['session']
+    #     task = modelTask.Task.objects.filter(id=pk)
+    #     form = forms.SolForm(request.POST)
+    #     data = form.data['textarea']
+    #     save = saveToFile.Save()
+    #     filePath = save.saveToFile(data)
+    #     run = runProgram.RunProgram()
+    #     afterRun = run.runFile(filePath, 0)
+    #     dict = {"data" : data}
+    #     dict['afterRun'] = afterRun
+    #     dict["task"] = task
+    #     dict['session'] = session
+    #     # todo сравнивать значения с realsolution
+    #     return render(request, "Solutions/checksol.html", dict)
